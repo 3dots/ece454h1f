8d8d28397e559e65ef2293663045ed6d2e8b2adc
3dot <viktor.riabtsev@utoronto.ca>
2014-11-18 20:42:54 -0500

    scripty

--- hw4/script.sh
+++ hw4/script.sh
@@ -1,12 +1,15 @@
+numProc=1
 
+for numProc in 1 2 4
 make clean
+rm rg rgs
 
 make randtrack_global_lock
-./randtrack 1 50 > rg
+./randtrack numProc 50 > rg
 sort -n rg > rgs
 
 diff r rg
-echo "Tested vs orginal"
+echo "Tested vs orginal with proc: " $numProc
 
 
 
