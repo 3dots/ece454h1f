6ca4fa615093bfd66d2493c4de98a673b4320d32
3dot <viktor.riabtsev@utoronto.ca>
2014-11-19 15:31:14 -0500

    list lock worked, testing element lock

--- hw4/script.sh
+++ hw4/script.sh
@@ -1,10 +1,10 @@
 make clean
-make randtrack_list_lock
+make randtrack_element_lock
 
 for numProc in 1 2 4
 do
 	rm rg rgs	
-	./randtrack_list_lock $numProc 50 > rg
+	./randtrack_element_lock $numProc 50 > rg
 	sort -n rg > rgs
 
 	diff rs rgs
