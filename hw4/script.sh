3b15c58f185c5471e5abdecfe8507d2a74343510
Riabtsev  Viktor <riabtse1@ug244.eecg>
2014-11-18 21:22:42 -0500

    Global lock is tested and working

--- hw4/script.sh
+++ hw4/script.sh
@@ -1,15 +1,16 @@
 
-numProc=1
- 
-make clean
-rm rg rgs
+for numProc in 1 2 4
+do
+	make clean
+	rm rg rgs
 
-make randtrack_global_lock
-./randtrack $numProc 50 > rg
-sort -n rg > rgs
+	make randtrack_global_lock
+	./randtrack_global_lock $numProc 50 > rg
+	sort -n rg > rgs
 
-diff rs rgs
-echo "Tested vs orginal with proc: $numProc"
+	diff rs rgs
+	echo "Tested vs orginal with proc: $numProc"
+done
 
 
 
